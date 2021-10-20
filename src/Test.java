2819011ae1db50c80a8512ba8bd06486e8184279
tnqkr3753 <tnqkr4610@gmail.com>
2021-10-21 02:06:01 +0900

    BackJoon 2108

--- src/Test.java
+++ src/Test.java
@@ -3,7 +3,11 @@ public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int sum = -9;
+		int len = 5;
+		int avg = sum/len;
+		System.out.println(avg);
+		System.out.println(Math.round(avg));
 	}
 
 }
